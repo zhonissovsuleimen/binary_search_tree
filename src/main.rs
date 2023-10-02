8a54519fa6b0ea51bc52e98ec771ee5174d4a090
Suleimen Zhonissov <lmfao159753@mail.ru>
2023-10-01 21:54:05 -0400

    user input, small type changes

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,39 @@
 use std::collections::HashSet;
 use rand::prelude::*;
+use std::io::stdin;
 
 fn main() {
+    // Getting user input
+    println!("Provide number of elements in the tree.");    
+    let size: i64;
+    loop{
+        let mut input_str = String::new();
+        stdin().read_line(&mut input_str).expect("Error reading console input");
+        match input_str.trim().parse::<i64>(){
+            Ok(input) => {
+                size = input; 
+                break;
+            }
+            Err(_) =>{
+                println!("Input is incorrect. Please provide unassigned 32 bit integer.");
+            }
+        }
+    }
+
+    let skewed = get_skewed_tree(size);
+    let balanced = get_balanced_tree(size);
 }
 
 
-fn get_balanced_tree(size: usize) -> Node{
+fn get_balanced_tree(size: i64) -> Node{
     let mut memory = HashSet::<i64>::new();
 
-    let rnd_root = rand::thread_rng().gen_range(i64::MIN..i64::MAX + 1);
+    let rnd_root = rand::thread_rng().gen_range(i64::MIN..=i64::MAX);
     let mut root =  Node::new(rnd_root);
 
-    let mut count: usize = 1; 
+    let mut count: i64 = 1; 
     while count < size{
-        let rnd = rand::thread_rng().gen_range(i64::MIN..i64::MAX + 1);
+        let rnd = rand::thread_rng().gen_range(i64::MIN..=i64::MAX);
         if memory.insert(rnd) {
             root.add(rnd);
             count += 1;
@@ -23,14 +43,15 @@ fn get_balanced_tree(size: usize) -> Node{
     root
 }
 
-fn get_skewed_tree(size: usize) -> Node{
+fn get_skewed_tree(size: i64) -> Node{
     let mut root = Node::new(1);
 
-    for i in 2..size{
-        root.add(i as i64);
+    for i in 2..=size{
+        root.add(i);
     }
     root
 }
+
 
 struct Node {
     value: i64,
@@ -61,5 +82,21 @@ impl Node {
             }
         }
 
+    }
+
+    fn find(&self, target: i64) -> bool{
+        let mut current_node = self;
+        loop{
+            let value = current_node.value;
+            if target == value{
+                return true;
+            }
+
+            let child_to_check = if target > value {&current_node.right} else {&current_node.left};
+            match child_to_check {
+                Some(child) => {current_node = child},
+                None => return false
+            } 
+        } 
     }
 }
